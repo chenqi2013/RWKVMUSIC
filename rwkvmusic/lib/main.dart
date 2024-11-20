a70bf9db8269af0afeda5afa15d9330b7516e3c2
chenqi <505991622@qq.com>
2024-11-20 18:09:24 +0800

    ncnn version

--- rwkvmusic/lib/main.dart
+++ rwkvmusic/lib/main.dart
@@ -357,7 +357,7 @@ void fetchABCDataByIsolate() async {
       debugPrint('load model fail,userIsolate!.kill()');
       ConfigStore.to.saveDeviceOnlyNCNN();
       currentModelType = ModelType.ncnn;
-      appVersion = 'ncnn_1.6.1_20241108';
+      appVersion = 'ncnn' + appVersionNumber;
       fetchABCDataByIsolate();
     } else if (data.toString().startsWith('tokens')) {
       isClicking = false;
