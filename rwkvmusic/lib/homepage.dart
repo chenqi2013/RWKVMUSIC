5e15af6a5a8899501dfe157a06f5ef21f7a8fcc8
chenqi <505991622@qq.com>
2025-04-11 16:18:51 +0800

    随机律动，播放，版本号，键盘

--- rwkvmusic/lib/homepage.dart
+++ rwkvmusic/lib/homepage.dart
@@ -78,7 +78,7 @@ RxBool isdownloading = false.obs;
 bool isFirstOpen = true;
 bool isOnlyNCNN = false;
 
-String appVersionNumber = '_1.6.1_20241223';
+String appVersionNumber = '_1.6.2_20250411';
 String appVersion = 'ncnn' + appVersionNumber;
 
 class _HomePageState extends State<HomePage> {
--- rwkvmusic/lib/utils/justaudioplayer.dart
+++ rwkvmusic/lib/utils/justaudioplayer.dart
@@ -1,6 +1,6 @@
-// import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:get/get.dart';
-import 'package:just_audio/just_audio.dart'; //windows报错，找不到方法
+// import 'package:just_audio/just_audio.dart'; //windows报错，找不到方法
 
 class JustAudioPlayerManage {
   static final JustAudioPlayerManage _instance =
@@ -9,7 +9,7 @@ class JustAudioPlayerManage {
   late AudioPlayer audioPlayer;
   var isMP3Playing = false.obs;
   JustAudioPlayerManage._internal() {
-    audioPlayer = AudioPlayer();
+    audioPlayer = AudioPlayer()..setReleaseMode(ReleaseMode.stop);
     // audioPlayer.onPlayerStateChanged.listen((state) {
     //   if (state == PlayerState.playing) {
     //     isMP3Playing.value = true;
@@ -20,27 +20,27 @@ class JustAudioPlayerManage {
   }
 
   Future<void> playAudio(String path) async {
-    // //方案1 使用audioplayers库
-    // await audioPlayer.resume();
-    // await audioPlayer.play(AssetSource(path), mode: PlayerMode.mediaPlayer);
+    //方案1 使用audioplayers库
+    await audioPlayer.stop();
+    await audioPlayer.play(AssetSource(path));
 
-    //方案二 使用just_audio库
-    // await audioPlayer.setFilePath(
-    //     'assets/player/soundfont/acoustic_guitar_steel-mp3/A0.mp3');
-    await audioPlayer
-        .setAudioSource(AudioSource.uri(Uri.parse('asset:///assets/$path')));
-    // await audioPlayer.setAudioSource(
-    //     AudioSource.uri(Uri.parse('asset:///assets/player/test.mp3')));
+    // //方案二 使用just_audio库
+    // // await audioPlayer.setFilePath(
+    // //     'assets/player/soundfont/acoustic_guitar_steel-mp3/A0.mp3');
+    // await audioPlayer
+    //     .setAudioSource(AudioSource.uri(Uri.parse('asset:///assets/$path')));
+    // // await audioPlayer.setAudioSource(
+    // //     AudioSource.uri(Uri.parse('asset:///assets/player/test.mp3')));
 
-    await audioPlayer.setClip(
-        start: const Duration(seconds: 0),
-        end: const Duration(milliseconds: 500));
-    await audioPlayer.play();
+    // await audioPlayer.setClip(
+    //     start: const Duration(seconds: 0),
+    //     end: const Duration(milliseconds: 500));
+    // await audioPlayer.play();
     print('playAudio');
   }
 
   Future<void> stopAudio() async {
     print('stopAudio');
-    await audioPlayer.pause();
+    await audioPlayer.stop();
   }
 }
